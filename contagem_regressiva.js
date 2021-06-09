54fbb8fffa355cc107e9e9920d8aaed83051281b
Mariana Sá <mariana_medice@hotmail.com>
2021-06-09 20:53:02 -0300

    Contagem regressiva

---
+++ contagem_regressiva.js
@@ -0,0 +1,11 @@
+function solucao(numero) { 
+	
+    let n = numero;
+    while (n > 0) {
+        console.log(n);
+        n--;
+    }
+    if (n == 0)
+    console.log(n); 
+    console.log("KABUM");      
+}
